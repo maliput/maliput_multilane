b0eb98167232c0dc4e5884ad837fce1fedffc195
Franco Cipollone <franco.c@ekumenlabs.com>
2021-01-18 10:44:11 -0300

    Adds tests for RoadNetworkLoader multilane plugin.

---
+++ test/plugin/road_network_plugin_test.cc
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include "maliput/plugin/maliput_plugin.h"
+#include "maliput/plugin/maliput_plugin_manager.h"
+#include "maliput/plugin/maliput_plugin_type.h"
+#include "maliput/plugin/road_network_loader.h"
+#include "maliput_multilane/road_geometry.h"
+
+#include "maliput/common/filesystem.h"
+
+namespace maliput {
+namespace multilane {
+namespace {
+
+GTEST_TEST(RoadNetworkLoader, VerifyRoadNetworkPlugin) {
+  // Get absolute path to a yaml file to be used for testing.
+  const std::string MULTILANE_RESOURCE_ROOT{"MULTILANE_RESOURCE_ROOT"};
+  const std::string kFileName{"/2x2_intersection.yaml"};
+  const std::string env_path = maliput::common::Filesystem::get_env_path(MULTILANE_RESOURCE_ROOT);
+  ASSERT_TRUE(!env_path.empty());
+
+  // RoadNetworkLoader plugin id.
+  const plugin::MaliputPlugin::Id kMultilanePluginId{"maliput_multilane"};
+  // Multilane properties needed for loading a road geometry.
+  const std::map<std::string, std::string> rg_multilane_properties{{"yaml_file", env_path + kFileName}};
+
+  // Check MaliputPlugin existence.
+  plugin::MaliputPluginManager manager{};
+  plugin::MaliputPlugin const* rn_plugin{nullptr};
+  EXPECT_NO_THROW(rn_plugin = manager.GetPlugin(kMultilanePluginId));
+  EXPECT_NE(nullptr, rn_plugin);
+
+  // Check multilane plugin is obtained.
+  EXPECT_EQ(kMultilanePluginId.string(), rn_plugin->GetId());
+  EXPECT_EQ(plugin::MaliputPluginType::kRoadNetworkLoader, rn_plugin->GetType());
+  std::unique_ptr<maliput::plugin::RoadNetworkLoader> rn_loader{nullptr};
+  EXPECT_NO_THROW(rn_loader = rn_plugin->ExecuteSymbol<std::unique_ptr<plugin::RoadNetworkLoader>>(
+                      plugin::RoadNetworkLoader::GetEntryPoint()));
+  EXPECT_NE(nullptr, rn_loader);
+
+  // Check multilane RoadNetwork is constructible.
+  std::unique_ptr<const maliput::api::RoadNetwork> rn{nullptr};
+  EXPECT_NO_THROW(rn = (*rn_loader)(rg_multilane_properties));
+  EXPECT_NE(nullptr, rn);
+  auto multilane_rg = dynamic_cast<const RoadGeometry*>(rn->road_geometry());
+  EXPECT_NE(nullptr, multilane_rg);
+}
+
+}  // namespace
+}  // namespace multilane
+}  // namespace maliput

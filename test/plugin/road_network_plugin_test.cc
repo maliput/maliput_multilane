5427172df601068d7b8fd4153f3d74efa2a7a3d8
Franco Cipollone <franco.c@ekumenlabs.com>
2021-01-18 17:35:10 -0300

    Addresses comments.

--- test/plugin/road_network_plugin_test.cc
+++ test/plugin/road_network_plugin_test.cc
@@ -1,16 +1,17 @@
-#include <gtest/gtest.h>
-
+// Copyright 2021 Toyota Research Institute
 #include <map>
 #include <memory>
 #include <string>
 
+#include <gtest/gtest.h>
+
+#include "maliput/common/filesystem.h"
 #include "maliput/plugin/maliput_plugin.h"
 #include "maliput/plugin/maliput_plugin_manager.h"
 #include "maliput/plugin/maliput_plugin_type.h"
 #include "maliput/plugin/road_network_loader.h"
-#include "maliput_multilane/road_geometry.h"
 
-#include "maliput/common/filesystem.h"
+#include "maliput_multilane/road_geometry.h"
 
 namespace maliput {
 namespace multilane {
@@ -30,22 +31,19 @@ GTEST_TEST(RoadNetworkLoader, VerifyRoadNetworkPlugin) {
 
   // Check MaliputPlugin existence.
   plugin::MaliputPluginManager manager{};
-  plugin::MaliputPlugin const* rn_plugin{nullptr};
-  EXPECT_NO_THROW(rn_plugin = manager.GetPlugin(kMultilanePluginId));
+  const plugin::MaliputPlugin* rn_plugin{manager.GetPlugin(kMultilanePluginId)};
   ASSERT_NE(nullptr, rn_plugin);
 
   // Check multilane plugin is obtained.
-  std::unique_ptr<maliput::plugin::RoadNetworkLoader> rn_loader{nullptr};
-  if (rn_plugin != nullptr) {
-    EXPECT_EQ(kMultilanePluginId.string(), rn_plugin->GetId());
-    EXPECT_EQ(plugin::MaliputPluginType::kRoadNetworkLoader, rn_plugin->GetType());
-    EXPECT_NO_THROW(rn_loader = rn_plugin->ExecuteSymbol<std::unique_ptr<plugin::RoadNetworkLoader>>(
-                        plugin::RoadNetworkLoader::GetEntryPoint()));
-  }
+  std::unique_ptr<maliput::plugin::RoadNetworkLoader> rn_loader;
+  EXPECT_EQ(kMultilanePluginId.string(), rn_plugin->GetId());
+  EXPECT_EQ(plugin::MaliputPluginType::kRoadNetworkLoader, rn_plugin->GetType());
+  EXPECT_NO_THROW(rn_loader = rn_plugin->ExecuteSymbol<std::unique_ptr<plugin::RoadNetworkLoader>>(
+                      plugin::RoadNetworkLoader::GetEntryPoint()));
   ASSERT_NE(nullptr, rn_loader);
 
   // Check multilane RoadNetwork is constructible.
-  std::unique_ptr<const maliput::api::RoadNetwork> rn{nullptr};
+  std::unique_ptr<const maliput::api::RoadNetwork> rn;
   EXPECT_NO_THROW(rn = (*rn_loader)(rg_multilane_properties));
   ASSERT_NE(nullptr, rn);
   auto multilane_rg = dynamic_cast<const RoadGeometry*>(rn->road_geometry());

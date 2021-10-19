993f1838da5d7defcbe241a5331593820373418b
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2021-10-19 08:52:44 -0300

    Extends RoadNetworkLoader plugin to include yaml descriptions and OnrampMerge implementation. (#76)
    Extends RoadNetworkLoader plugin to allow:
    
     - Loading a RoadNetwork it from a yaml file.
     - Loading a RoadNetwork from a yaml descriptions.
     - Loading a RoadNetwork using the MultilaneOnRampeMerge implementation.

--- src/plugin/road_network.cc
+++ src/plugin/road_network.cc
@@ -1,8 +1,10 @@
 // Copyright 2021 Toyota Research Institute
 #include <memory>
 
+#include <maliput/common/maliput_abort.h>
 #include <maliput/plugin/road_network_loader.h>
 
+#include "maliput_multilane/multilane_onramp_merge.h"
 #include "maliput_multilane/road_network_builder.h"
 
 namespace maliput {
@@ -14,19 +16,66 @@ namespace {
 // @param parameters  A dictionary of properties to fill in a multilane::RoadNetworkConfiguration struct.
 //                    Keys are the names of attributes in multilane::RoadNetworkConfiguration.
 // @returns A multilane::RoadNetworkConfiguration.
-maliput::multilane::RoadNetworkConfiguration GetPropertiesFromStringMap(
+maliput::multilane::RoadNetworkConfiguration GetRoadNetworkPropertiesFromStringMap(
     const std::map<std::string, std::string>& parameters) {
   auto it = parameters.find("yaml_file");
   const std::string yaml_file = it != parameters.end() ? it->second : "";
-  return {yaml_file};
+
+  it = parameters.find("yaml_description");
+  const std::string yaml_description = it != parameters.end() ? it->second : "";
+  return {yaml_file, yaml_description};
+}
+
+// Return a multilane::MultilaneRoadCharacteristics object out of a map of strings.
+// @param parameters  A dictionary of properties to fill in a multilane::MultilaneRoadCharacteristics struct.
+//                    Keys are the names of attributes in multilane::MultilaneRoadCharacteristics.
+// @returns A multilane::MultilaneRoadCharacteristics.
+maliput::multilane::MultilaneRoadCharacteristics GetOnRampPropertiesFromStringMap(
+    const std::map<std::string, std::string>& parameters) {
+  MultilaneRoadCharacteristics default_on_ramp_params{};
+  auto it = parameters.find("lane_width");
+  const double lane_width = it != parameters.end() ? std::stod(it->second) : default_on_ramp_params.lane_width;
+
+  it = parameters.find("left_shoulder");
+  const double left_shoulder = it != parameters.end() ? std::stod(it->second) : default_on_ramp_params.left_shoulder;
+
+  it = parameters.find("right_shoulder");
+  const double right_shoulder = it != parameters.end() ? std::stod(it->second) : default_on_ramp_params.right_shoulder;
+
+  it = parameters.find("lane_number");
+  const int lane_number = it != parameters.end() ? std::stoi(it->second) : default_on_ramp_params.lane_number;
+
+  return {lane_width, left_shoulder, right_shoulder, lane_number};
+}
+
+// Builds a maliput::api::RoadNetwork depending on `properties`.
+// A 'road_network_source' flag is used to differentiate which multilane implementation will be used:
+//    1. The base multilane builder loaded from a yaml file or a serialized yaml description.
+//    2. The MultilaneOnRampMerge builder.
+std::unique_ptr<maliput::api::RoadNetwork> BuildRoadNetworkFromParams(
+    const std::map<std::string, std::string>& properties) {
+  auto it = properties.find("road_network_source");
+  if (it == properties.end()) {
+    MALIPUT_THROW_MESSAGE("'road_network_source' parameter is missing: 'yaml' or 'on_ramp_merge' should be selected.");
+  }
+  const std::string source = it->second;
+  if (source == "yaml") {
+    return maliput::multilane::BuildRoadNetwork(GetRoadNetworkPropertiesFromStringMap(properties));
+  } else if (source == "on_ramp_merge") {
+    return maliput::multilane::BuildOnRampMergeRoadNetwork(GetOnRampPropertiesFromStringMap(properties));
+  } else {
+    MALIPUT_THROW_MESSAGE(
+        "'road_network_source' parameter doesn't contain a valid source: 'yaml' or 'on_ramp_merge' should be "
+        "selected.");
+  }
 }
 
 // Implementation of a maliput::plugin::RoadNetworkLoader using multilane backend.
 class RoadNetworkLoader : public maliput::plugin::RoadNetworkLoader {
  public:
-  std::unique_ptr<const maliput::api::RoadNetwork> operator()(
+  std::unique_ptr<maliput::api::RoadNetwork> operator()(
       const std::map<std::string, std::string>& properties) const override {
-    return maliput::multilane::BuildRoadNetwork(GetPropertiesFromStringMap(properties));
+    return BuildRoadNetworkFromParams(properties);
   }
 };
 
